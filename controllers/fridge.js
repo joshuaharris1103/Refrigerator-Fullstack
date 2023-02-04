fee64e5234cf9dd698d9523f04a63a8878e28358
jrh <joshuaharris1103@gmail.com>
2023-02-03 19:31:02 -0500

    fixed all of the routes finally

--- controllers/fridge.js
+++ controllers/fridge.js
@@ -8,9 +8,9 @@ const router = express.Router()
 
 // Routes
 
-// Home Screen for non user
+// index ALL
 router.get('/', (req, res) => {
-	Fridge.find({ owner: req.session.userId })		
+	Fridge.find({})
 		.then(fridge => {
 			const username = req.session.username
 			const loggedIn = req.session.loggedIn
@@ -22,124 +22,33 @@ router.get('/', (req, res) => {
 		})
 })
 
-router.get('/json', (req, res) => {
-	Fridge.find({})
-		.then(fridge => {
-			const username = req.session.username
-			const loggedIn = req.session.loggedIn
-			
-			res.json(fridge)
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// // Homescreen for user
-router.get('/:owner/fridge', (req, res) => {
+// index that shows only the user's fridge
+router.get('/mine', (req, res) => {
+    // destructure user info from req.session
     const { username, userId, loggedIn } = req.session
 	Fridge.find({ owner: userId })
 		.then(fridge => {
-			res.render('/fridge/index', { fridge, username, loggedIn })
+			res.render('fridge/index', { fridge, username, loggedIn })
 		})
 		.catch(error => {
 			res.redirect(`/error?error=${error}`)
 		})
 })
 
-// // GET route that renders our page with the form for new items
+// new route -> GET route that renders our page with the form
 router.get('/new', (req, res) => {
 	const { username, userId, loggedIn } = req.session
 	res.render('fridge/new', { username, loggedIn })
 })
 
-// // CREATE (fridge/new) -> POST route that actually calls the db and makes a new document
+// create -> POST route that actually calls the db and makes a new document
 router.post('/', (req, res) => {
+	req.body.ready = req.body.ready === 'on' ? true : false
+
 	req.body.owner = req.session.userId
-	// req.body.ready = req.body.ready === 'on' ? true : false
-	const newItem = req.body
-	Fridge.create(newItem)
-	.then(fridge => {
-			console.log('this is req.body aka newItem, after owner\n', fridge)
-			res.redirect(`fridge/`)
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-
-// // This Get route allows us to SEE edit form for specific item
-router.get('/:id', (req, res) => {
-	const fridgeId = req.body.id
-	Fridge.findById(fridgeId)
+	Fridge.create(req.body)
 		.then(fridge => {
-			const {username, loggedIn, userId} = req.session
-			res.render('fridge/edit', { fridge, ...req.session})
-			console.log('EDIT FORM HAS BEEN CREATED')
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-// // PUT route used to update sepcific item
-router.put('/edit/:id', (req, res) => {
-	const fridgeId = req.params.id
-	console.log('this id is:', fridgeId)
-	Fridge.findById(fridgeId)
-		.then(fridge => {
-			if (fridge.owner == req.session.userId){
-				console.log('This item was successfully updated')
-				return Fridge.findOneAndUpdate(req.body)
-			} else {
-				res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20edit%20this%20item`)
-				console.log('this is the redirect')
-			}
-		})
-		.then(() => {
-			res.redirect(`/fridge/edit/${fridgeId}`)
-			console.log('this item has been updated')
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-router.get('/edit/:id', (req, res) => {
-	const fridgeId = req.params.id
-	Fridge.findById(fridgeId)
-		.then(fridge => {
-			console.log('ITEM EDIT HAS BEEN MADE')
-			res.redirect(`../${fridgeId}`)
-		})
-		.catch((error) => {
-			console.log('ITEM EDIT HAS FAILED')
-			res.redirect(`/error?error=${error}`)
-		})
-})
-// router.get('fridge/item', (req, res) => {
-// 	res.json('hello world')
-// 	const { username, userId, loggedIn } = req.session
-// 	res.render('fridge/item', { username, loggedIn })
-// })
-
-
-
-
-// delete route
-router.delete('/:id', (req, res) => {
-	const fridgeId = req.params.id
-	Fridge.findById(fridgeId)
-		.then(fridge => {
-			console.log('deleted')
-			if (fridge.owner == req.session.userId){
-				console.log('delete if statement hit')
-				return fridge.deleteOne()
-			} else {
-				res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20delete%20this%20item`)
-			}
-		})
-		.then(() => {
+			console.log('this was returned from create', fridge)
 			res.redirect('/fridge')
 		})
 		.catch(error => {
@@ -147,18 +56,56 @@ router.delete('/:id', (req, res) => {
 		})
 })
 
-//SHOW
-// router.get("/:id", (req, res) => {
-//     const id = req.params.id
-//     Fridge.findById(id)
-// 		.populate('username')
-//         .then(fridge => {
-//             res.json({ fridge: fridge})
-//         })
-// 		.catch((error) => {
-// 			res.redirect(`/error?error=${error}`)
-// 		})
-// })
+// edit route -> GET that takes us to the edit form view
+router.get('/:id/edit', (req, res) => {
+	const fridgeId = req.params.id
+	Fridge.findById(fridgeId)
+		.then(fridge => {
+			res.render('fridge/edit', { fridge })
+		})
+		.catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+// update route
+router.put('/:id', (req, res) => {
+	const fridgeId = req.params.id
+	// req.body.ready = req.body.ready === 'on' ? true : false
+
+	Fridge.findByIdAndUpdate(fridgeId, req.body, { new: true })
+		.then(fridge => {
+			res.redirect(`/fridge/`)
+		})
+		.catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+// show route
+router.get('/:id', (req, res) => {
+	const fridgeId = req.params.id
+	Fridge.findById(fridgeId)
+		.then(fridge => {
+            const {username, loggedIn, userId} = req.session
+			res.render('fridge/show', { fridge, username, loggedIn, userId })
+		})
+		.catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+// delete route
+router.delete('/:id', (req, res) => {
+	const fridgeId = req.params.id
+	Fridge.findByIdAndRemove(fridgeId)
+		.then(fridge => {
+			res.redirect('/fridge')
+		})
+		.catch(error => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
 
 // Export the Router
 module.exports = router
