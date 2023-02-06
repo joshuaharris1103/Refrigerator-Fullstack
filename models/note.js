13e226438b011e56bacd2dc63d029377ca99d568
jrh <joshuaharris1103@gmail.com>
2023-02-05 22:40:22 -0500

    attempting to implement note submodel

--- models/note.js
+++ models/note.js
@@ -3,13 +3,8 @@
 // /////////////////////////////////////////////////////////
 // const mongoose = require('../utils/middleware')
 
-// // All we need from mongoose, to build subdocuments
-// // is the schema constructor. 
-// // SUBDOCUMENTS ARE NOT MONGOOSE MODELS.
-// // we'll destructure the Schema function from mongoose
 // const { Schema } = mongoose
 
-// // note schema
 // const noteSchema = new Schema({
 //     note: {
 //         type: String,
@@ -20,11 +15,7 @@
 //         ref: 'User',
 //         required: true
 //     }
-// }, {
-//     timestamps: true
-// })
-
-// // Take note that there is no model function happening anywhere in this file. That's because SUBDOCS ARE NOT MONGOOSE MODELS.
+// }, { timestamps: true })
 
 // ////////////////////////////////////
 // //// Export our Schema          ////
